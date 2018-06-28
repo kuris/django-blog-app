dc3fbf7441c5aafe0628428bf8da01bbf87f97af
kosta <kosta@kosta.co.kr>
2018-06-28 15:44:42 +0900

    django blog app first commit

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -122,7 +122,6 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -130,3 +129,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'static')
+LOGIN_REDIRECT_URL='/'
